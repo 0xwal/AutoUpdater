361ff1053b0921c3f14d5675fecd854720b22820
BISOON <bison.b7@gmail.com>
2017-01-04 21:58:58 +0300

    AutoUpdater Class
    This is AutoUpdater Class Simple AutoUpdater With MsgBox Without GUI

---
+++ AutoUpdater.cs
@@ -0,0 +1,9 @@
+using System;
+
+
+class AutoUpdater
+{
+	//We make a version as string because some language will not work for point ( double / float )
+	private string _version;
+	private string _desc;
+}
